35223758266115f222d0f1fab51ae390e77c1599
rajprasath007 <rajprasath4321@gmail.com>
2025-03-25 18:41:42 +0530

    Renamed start.sh to start.bat

--- Fabfood/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz
-tar -xvzf apache-tomcat-9.0.80.tar.gz
-mv apache-tomcat-9.0.80 tomcat
-cp target/*.war tomcat/webapps/
-chmod +x tomcat/bin/catalina.sh
-./tomcat/bin/catalina.sh run
